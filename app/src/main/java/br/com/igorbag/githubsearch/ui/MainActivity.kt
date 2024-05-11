aeee5a345344d78b3e16d03b6d8280090f6e0e69
Lucca Cruz <114677146+luccaseabra@users.noreply.github.com>
2024-05-11 11:16:08 +0100

    Implementation of functions setupView, setupListeners, saveUserLocal, showUserName and modifications on activity_main layout (et_nome_usuario).

--- app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.igorbag.githubsearch.ui
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -13,38 +14,55 @@ import br.com.igorbag.githubsearch.domain.Repository
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var nomeUsuario: EditText
-    lateinit var btnConfirmar: Button
-    lateinit var listaRepositories: RecyclerView
+    lateinit var userName: EditText
+    lateinit var btnConfirm: Button
+    lateinit var repositoriesList: RecyclerView
     lateinit var githubApi: GitHubService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupView()
-        showUserName()
         setupRetrofit()
-        getAllReposByUserName()
+        setupView()
+        setupListeners()
+        showUserName()
+
+        //getAllReposByUserName()
     }
 
     // Metodo responsavel por realizar o setup da view e recuperar os Ids do layout
     fun setupView() {
-        //@TODO 1 - Recuperar os Id's da tela para a Activity com o findViewById
+        userName = findViewById(R.id.et_nome_usuario)
+        btnConfirm = findViewById(R.id.btn_confirmar)
+        repositoriesList = findViewById(R.id.rv_lista_repositories)
     }
 
     //metodo responsavel por configurar os listeners click da tela
     private fun setupListeners() {
-        //@TODO 2 - colocar a acao de click do botao confirmar
+        btnConfirm.setOnClickListener{
+            val inputUserName = userName.text.toString()
+            saveUserLocal(inputUserName)
+        }
+
     }
 
 
     // salvar o usuario preenchido no EditText utilizando uma SharedPreferences
-    private fun saveUserLocal() {
-        //@TODO 3 - Persistir o usuario preenchido na editText com a SharedPref no listener do botao salvar
+    private fun saveUserLocal(value: String) {
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()){
+            putString("Username", value)
+            apply()
+        }
     }
 
     private fun showUserName() {
-        //@TODO 4- depois de persistir o usuario exibir sempre as informacoes no EditText  se a sharedpref possuir algum valor, exibir no proprio editText o valor salvo
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        val username = sharedPreferences.getString("Username", "") ?: ""
+
+        if(username.isNotEmpty()) {
+            userName.setText(username)
+        }
     }
 
     //Metodo responsavel por fazer a configuracao base do Retrofit
